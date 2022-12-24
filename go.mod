a403d1ceb19010f7e354ff2b71e7d5d509e4bceb
Sato Ryoka <satoryoka0920@gmail.com>
2022-12-25 07:48:23 +0900

    add: run.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/orange-bp-dev/go_todo_app
 
 go 1.16
+
+require golang.org/x/sync v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"context"
+	"log"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	)
-
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
