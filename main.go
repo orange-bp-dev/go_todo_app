a087ff77d8a24fd2b2e1484d893a2419e3929053
Sato Ryoka <satoryoka0920@gmail.com>
2022-12-25 12:10:11 +0900

    add: port flexible

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"context"
 	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port nmber\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
--- run.go
+++ run.go
@@ -4,23 +4,22 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: "18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Helo, %s", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
 
-
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
